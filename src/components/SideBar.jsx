a141c0ab3477603b0e4f7f0a9ed5e1c6ad7582ab
Vladimir <bigcorvet@gmail.com>
2023-06-14 09:11:05 +0300

    added Sidebar component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,64 +1,27 @@
-import React from 'react'
-import news1 from '../assets/images/news/news1.jpeg'
-import {CiClock2} from 'react-icons/ci'
+import React from "react";
+import news1 from "../assets/images/news/news1.jpeg";
+import { CiClock2 } from "react-icons/ci";
+import { news } from "../data/news";
 
 export default function SideBar() {
   return (
-    <div className='sidebar'>
-        <div className='sidebar-wrapper'>
-            <p className='sidebar-heading'>НОВИНИ</p>
-            <hr />
-            <div className='sidebar-item'>
-                <div className='sidebar-item__content'>
-                    <img src={news1} alt='news1' className='img-fluid sidebar-img'/>
-                    <p>Годують до 3 тисяч людей на добу: Порошенко дорогою з фронту заїхав у відоме волонтерське кафе</p>
-                </div>
-                <div className='sidebar-item__time'>
-                    <CiClock2 style={{fontSize:'12px', marginRight:'7px'}}/>
-                    <span style={{fontSize:'12px'}}>03-06-2023</span>
-                </div>
+    <div className="sidebar">
+      <div className="sidebar-wrapper">
+        <p className="sidebar-heading">НОВИНИ</p>
+        <hr />
+        {news.map((item) => (
+          <div key={item.id} className="sidebar-item">
+            <div className="sidebar-item__content">
+              <img src={item.images[0]} alt="news1" className="img-fluid sidebar-img" />
+              <p>{item.title}</p>
             </div>
-            <div className='sidebar-item'>
-                <div className='sidebar-item__content'>
-                    <img src={news1} alt='news1' className='img-fluid sidebar-img'/>
-                    <p>Годують до 3 тисяч людей на добу: Порошенко дорогою з фронту заїхав у відоме волонтерське кафе</p>
-                </div>
-                <div className='sidebar-item__time'>
-                    <CiClock2 style={{fontSize:'12px', marginRight:'7px'}}/>
-                    <span style={{fontSize:'12px'}}>03-06-2023</span>
-                </div>
+            <div className="sidebar-item__time">
+              <CiClock2 style={{ fontSize: "12px", marginRight: "7px" }} />
+              <span style={{ fontSize: "12px" }}>{item.date}</span>
             </div>
-            <div className='sidebar-item'>
-                <div className='sidebar-item__content'>
-                    <img src={news1} alt='news1' className='img-fluid sidebar-img'/>
-                    <p>Годують до 3 тисяч людей на добу: Порошенко дорогою з фронту заїхав у відоме волонтерське кафе</p>
-                </div>
-                <div className='sidebar-item__time'>
-                    <CiClock2 style={{fontSize:'12px', marginRight:'7px'}}/>
-                    <span style={{fontSize:'12px'}}>03-06-2023</span>
-                </div>
-            </div>
-            <div className='sidebar-item'>
-                <div className='sidebar-item__content'>
-                    <img src={news1} alt='news1' className='img-fluid sidebar-img'/>
-                    <p>Годують до 3 тисяч людей на добу: Порошенко дорогою з фронту заїхав у відоме волонтерське кафе</p>
-                </div>
-                <div className='sidebar-item__time'>
-                    <CiClock2 style={{fontSize:'12px', marginRight:'7px'}}/>
-                    <span style={{fontSize:'12px'}}>03-06-2023</span>
-                </div>
-            </div>
-            <div className='sidebar-item'>
-                <div className='sidebar-item__content'>
-                    <img src={news1} alt='news1' className='img-fluid sidebar-img'/>
-                    <p>Годують до 3 тисяч людей на добу: Порошенко дорогою з фронту заїхав у відоме волонтерське кафе</p>
-                </div>
-                <div className='sidebar-item__time'>
-                    <CiClock2 style={{fontSize:'12px', marginRight:'7px'}}/>
-                    <span style={{fontSize:'12px'}}>03-06-2023</span>
-                </div>
-            </div>
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
