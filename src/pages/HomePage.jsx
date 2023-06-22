c08235c6882c156f7426ae80aa42e15f4525baee
Vladimir <bigcorvet@gmail.com>
2023-06-22 18:00:32 +0300

    added component Articles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import { Container , Row, Col} from 'react-bootstrap'
-import Slider from '../components/Slider'
-import SideBar from '../components/SideBar'
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Slider from "../components/Slider";
+import SideBar from "../components/SideBar";
+import Map from "../components/Map";
+import BlockVideo from "../components/BlockVideo";
+import Articles from "../components/Articles";
 
 export default function HomePage() {
   return (
-    <div className='home'>
-       <Container className='home-container'>
-      <Row>
-        <Col md={8}>
-          <div className='slider-wrapper'>
+    <div className="home">
+      <Container className="home-container">
+        <Row>
+          <Col md={8}>
+            <div className="slider-wrapper">
               <Slider />
-          </div>
-        </Col>
-        <Col md={4}>
-          <SideBar />
-        </Col>
-      </Row>
-    </Container>
+            </div>
+            <Map /> 
+            <BlockVideo />
+          </Col>
+          <Col md={4}>
+            <SideBar />
+          </Col>
+        </Row>
+          <Articles />
+      </Container>
     </div>
-   
-  )
+  );
 }
