e005fb906daca55640a16d5888ca510f215ea0e3
Vladimir <bigcorvet@gmail.com>
2023-07-03 14:05:13 +0300

    create router for App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import Articles from "./pages/Articles";
+import SinglePublication from "./pages/SinglePublication";
+import HomePage from "./pages/HomePage";
+import News from './pages/News'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "eurosolidarity/category/articles",
+        element: <Articles />,
+      },
+      {
+        path: "/eurosolidarity/category/:segment/:id",
+        element: <SinglePublication />,
+      },
+      {
+        path: "/eurosolidarity/category/news",
+        element: <News />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
