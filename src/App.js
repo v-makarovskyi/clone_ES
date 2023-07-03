635bb47f1148914b4bcdd035b87511e86571fafd
Vladimir <bigcorvet@gmail.com>
2023-07-03 14:09:15 +0300

    added Ourlet component RRD

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/reset.scss";
 import "../src/style.scss";
 import { Container } from "react-bootstrap";
-import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Articles from "./pages/Articles";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />
-  }
-])
+
 
 
 function App() {
   return (
     <div className="App">
-       <RouterProvider router={router} />
+      <NavBar />
+      <div id="detail">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
    
   );
