f17322c9112851bca5b05281111ab86dbd3d4bf8
Vladimir <bigcorvet@gmail.com>
2023-07-03 17:14:31 +0300

    added heading for small ListPublications

--- src/components/ListPublications.jsx
+++ src/components/ListPublications.jsx
@@ -13,7 +13,16 @@ export default function ListPublications({
 }) {
   return (
     <div className="publications">
-      <p className="publications-heading">{title}</p>
+      {big ? (
+        <p className="publications-heading">{title}</p>
+      ) : (
+        <>
+        <p className="blockVideo-heading">{title}</p>
+        <hr />
+        </>
+        
+      )}
+
       <Row>
         {content.slice(0, articlesQty).map((c) => (
           <Publication
